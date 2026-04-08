8fa3f776db04d524a6fa2aca423fe65b18857594
ommprakash48 <ommprakash6046@gmail.com>
2026-04-08 23:01:19 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,26 +1,87 @@
 const container = document.getElementById("products-container");
 const loading = document.getElementById("loading");
 
-// Fetch API
+const searchInput = document.getElementById("search");
+const categorySelect = document.getElementById("category");
+const sortSelect = document.getElementById("sort");
+
+let allProducts = [];
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+
 fetch("https://dummyjson.com/products")
   .then(res => res.json())
   .then(data => {
-    loading.style.display = "none"; // hide loading
-    displayProducts(data.products);
-  })
-  .catch(err => {
-    loading.innerText = "Failed to load data";
-    console.error(err);
+    loading.style.display = "none";
+    allProducts = data.products;
+
+    displayProducts(allProducts);
+    loadCategories(allProducts);
   });
 
-// Display function
+
 function displayProducts(products) {
   container.innerHTML = products.map(product => `
     <div class="product">
-      <img src="${product.thumbnail}" alt="${product.title}">
+      <img src="${product.thumbnail}">
       <h3>${product.title}</h3>
-      <p>${product.description.substring(0, 60)}...</p>
-      <p class="price">$${product.price}</p>
+      <p>$${product.price}</p>
+      <button onclick="toggleFavorite(${product.id})">
+        ${favorites.includes(product.id) ? "❤️" : "🤍"}
+      </button>
     </div>
   `).join("");
 }
+
+
+function loadCategories(products) {
+  const categories = [...new Set(products.map(p => p.category))];
+
+  categorySelect.innerHTML += categories.map(cat => `
+    <option value="${cat}">${cat}</option>
+  `).join("");
+}
+
+
+function applyFilters() {
+  let result = [...allProducts];
+
+
+  const searchText = searchInput.value.toLowerCase();
+  result = result.filter(p =>
+    p.title.toLowerCase().includes(searchText)
+  );
+
+
+  if (categorySelect.value !== "all") {
+    result = result.filter(p =>
+      p.category === categorySelect.value
+    );
+  }
+
+
+  if (sortSelect.value === "low") {
+    result = result.sort((a, b) => a.price - b.price);
+  } else if (sortSelect.value === "high") {
+    result = result.sort((a, b) => b.price - a.price);
+  }
+
+  displayProducts(result);
+}
+
+
+function toggleFavorite(id) {
+  if (favorites.includes(id)) {
+    favorites = favorites.filter(f => f !== id);
+  } else {
+    favorites.push(id);
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  applyFilters();
+}
+
+
+searchInput.addEventListener("input", applyFilters);
+categorySelect.addEventListener("change", applyFilters);
+sortSelect.addEventListener("change", applyFilters);
